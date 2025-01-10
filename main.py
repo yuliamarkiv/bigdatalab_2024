1671c0c556baf7836cbae37e181dec1939e91eac
Yuliia <72133073+yuliamarkiv@users.noreply.github.com>
2025-01-10 23:27:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from data_preprocessor.processor import DataPreprocessor
+from source.processor import DataPreprocessor
 
 
 
 if __name__ == "__main__":
-    input_directory = "data"
-    output_directory = "preprocessed_data"
-    preprocessor = DataPreprocessor(input_directory, output_directory)
-    preprocessor.main("chicago_taxi_data_jan_to_jun_2024.csv")
+    preprocessor = DataPreprocessor()
+    preprocessor.predict_data()
